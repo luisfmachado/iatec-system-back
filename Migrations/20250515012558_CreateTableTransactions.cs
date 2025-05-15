0e7adb127298d621821e9f90580766deac65aa44
luisfmachado <123130732+luisfmachado@users.noreply.github.com>
2025-05-14 22:31:47 -0300

    feat: Migration CreateTableTransaction

---
+++ Migrations/20250515012558_CreateTableTransactions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iatec_system_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<bool>(type: "bit", nullable: false),
+                    Value = table.Column<double>(type: "float", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
